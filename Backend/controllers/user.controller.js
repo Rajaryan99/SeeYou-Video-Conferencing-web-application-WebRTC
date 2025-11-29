17abbe84a565cc798438d08ca55194dfb840016e
Rajaryan singh <raj.aryan2518@gmail.com>
2025-11-29 12:54:21 +0530

    login route

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -29,39 +29,39 @@ const register = async (req, res) => {
 }
 
 
-// const login = async (req, res) => {
-//     const { username, password } = req.body;
+const login = async (req, res) => {
+    const { username, password } = req.body;
 
-//     if (!username || !password) {
-//         return res.status(400).json({ message: "Please provide username and password" })
-//     }
+    if (!username || !password) {
+        return res.status(400).json({ message: "Please provide username and password" })
+    }
 
-//     try {
-//         const user = await User.findOne({ username });
-//         if (!user) {
-//             return res.status(httpStatus.NOT_FOUND).json({ message: "User NOT_FOUND '_'" })
-//         }
-
-
-//         if (await bcrypt.compare(password, user.password)) {
-//             let token = crypto.randomBytes(20).toString('hex');
-
-//             user.token = token;
-//             user.tokenExpiry = Date.now() + 7 * 24 * 60 * 60 * 1000;
-//             await user.save();
-//              res.status(200).json({ message: "Login successful" });
-
-//         }
+    try {
+        const user = await User.findOne({ username });
+        if (!user) {
+            return res.status(httpStatus.NOT_FOUND).json({ message: "User NOT_FOUND '_'" })
+        }
 
 
+        if (await bcrypt.compare(password, user.password)) {
+            let token = crypto.randomBytes(20).toString('hex');
 
-//     } catch (error) {
-//         console.log('Auth error', error.message);
-//         res.status(500).json({ message: "Server error" });
+            user.token = token;
+            user.tokenExpiry = Date.now() + 7 * 24 * 60 * 60 * 1000;
+            await user.save();
+             res.status(200).json({ message: "Login successful" });
+
+        }
 
 
 
-//     }
-// }
+    } catch (error) {
+        console.log('Auth error', error.message);
+        res.status(500).json({ message: "Server error" });
+
+
+
+    }
+}
 
 export { register, login };
