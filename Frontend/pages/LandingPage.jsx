003b1ccc70573f55e80f083cf836a57ead30d54b
Rajaryan singh <raj.aryan2518@gmail.com>
2025-12-01 23:15:53 +0530

    hero Section

--- Frontend/pages/LandingPage.jsx
+++ Frontend/pages/LandingPage.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 export default function LandingPage() {
   return (
     <div className='landingPageContainer'>
+
+      {/* Navbar */}
       <nav>
           <div className="navHeader">
             <h2>See<span className='you'>You </span>'__'</h2>
@@ -10,11 +13,25 @@ export default function LandingPage() {
           <div className="navList">
             <p>Join as Guest</p>
             <p>Register</p>
-            <div role='button'>
+            <div role='button' className='btn'>
               <p>Login</p>
             </div>
           </div>
       </nav>
+
+{/* HeroSection */}
+      <div className="landingMainContainer">
+          <div className="leftSection">
+           <h1> <span style={{color: 'rgb(208, 2, 70)'}}>Connect</span> with your loved ones!!!</h1> 
+           <p>Cover a distance by SeeYou ...</p>
+           <div role='button' className='getStarted btn'>
+            <Link className='getStarted' to={'/home'}>Get Started</Link>
+           </div>
+          </div>
+          <div className="rightSection">
+            <img src="/mobile.png" alt="" />
+          </div>
+      </div>
     </div>
   )
 }
