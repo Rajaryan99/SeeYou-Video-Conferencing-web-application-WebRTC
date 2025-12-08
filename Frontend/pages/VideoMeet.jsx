456dd383e84ce9432f3b8dd4c33903e4d9d2b76c
Rajaryan singh <raj.aryan2518@gmail.com>
2025-12-08 22:12:55 +0530

    share screen feature

--- Frontend/pages/VideoMeet.jsx
+++ Frontend/pages/VideoMeet.jsx
@@ -206,7 +206,7 @@ export default function VideoMeet() {
   let black = ({ width = 640, height = 480 } = {}) => {
     let canvas = Object.assign(document.createElement("canvas"), { width, height });
 
-    canvas.getContext('2d'.fillRect(0, 0, width, height));
+    canvas.getContext('2d').fillRect(0, 0, width, height);
     let stream = canvas.captureStream();
     return Object.assign(stream.getVideoTracks()[0], { enabled: false })
   }
@@ -475,7 +475,7 @@ let getDislayMediaSuccess = (stream) => {
       if(navigator.mediaDevices.getDisplayMedia){
         navigator.mediaDevices.getDisplayMedia({video: true, audio: true})
         .then(getDislayMediaSuccess)
-        // .then((stream) => {})
+        // .then((stream) => { })
         .catch((e) => console.log(e))
       }
     }
