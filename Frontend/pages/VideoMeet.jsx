471c72e9d21af113dafaa85d36a3edd86465e7ae
Rajaryan singh <raj.aryan2518@gmail.com>
2025-12-08 13:24:34 +0530

    sharescreen

--- Frontend/pages/VideoMeet.jsx
+++ Frontend/pages/VideoMeet.jsx
@@ -424,6 +424,68 @@ export default function VideoMeet() {
         setAudio(!audio)
   }
 
+  const handleScreen = () => {
+    setScreen(!screen)
+  }
+
+let getDislayMediaSuccess = (stream) => {
+        try {
+            window.localStream.getTracks().forEach(track => track.stop())
+        } catch (e) { console.log(e) }
+
+        window.localStream = stream
+        localVideoRef.current.srcObject = stream
+
+        for (let id in connections) {
+            if (id === socketIdRef.current) continue
+
+            connections[id].addStream(window.localStream)
+
+            connections[id].createOffer().then((description) => {
+                connections[id].setLocalDescription(description)
+                    .then(() => {
+                        socketRef.current.emit('signal', id, JSON.stringify({ 'sdp': connections[id].localDescription }))
+                    })
+                    .catch(e => console.log(e))
+            })
+        }
+
+        stream.getTracks().forEach(track => track.onended = () => {
+            setScreen(false)
+
+            try {
+                let tracks = localVideoRef.current.srcObject.getTracks()
+                tracks.forEach(track => track.stop())
+            } catch (e) { console.log(e) }
+
+            let blackSilence = (...args) => new MediaStream([black(...args), silence()])
+            window.localStream = blackSilence()
+            localVideoRef.current.srcObject = window.localStream
+
+            getUserMedia()
+
+        })
+    }
+
+
+
+
+  const getDisplayMedia = () => {
+    if(screen) {
+      if(navigator.mediaDevices.getDisplayMedia){
+        navigator.mediaDevices.getDisplayMedia({video: true, audio: true})
+        .then(getDislayMediaSuccess)
+        // .then((stream) => {})
+        .catch((e) => console.log(e))
+      }
+    }
+  }
+
+  useEffect(() => {
+    if(screen !== undefined){
+      getDisplayMedia();
+    }
+  }, [screen])
 
   let getMedia = () => {
     setVideo(videoAvailable);
@@ -439,6 +501,7 @@ export default function VideoMeet() {
 
 
   return (
+
     <div>
 
 
@@ -468,7 +531,7 @@ export default function VideoMeet() {
                       </IconButton>
 
                       {screenAvailable === true ? 
-                      <IconButton style={{color: "#fff"}}>
+                      <IconButton onClick={handleScreen} style={{color: "#fff"}}>
                         {screen === true ? <ScreenShareIcon/> : <StopScreenShareIcon/> }
                       </IconButton> : <></>}
 
@@ -504,5 +567,8 @@ export default function VideoMeet() {
 
 
     </div>
+  
   )
+
+
 }
